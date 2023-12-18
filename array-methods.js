6b270e732a4a0b6b0d9959ba228b5722eb851c97
unknown <JNKarif>
2023-12-18 22:45:49 +0600

    concat and push method

--- array-methods.js
+++ array-methods.js
@@ -1,14 +1,17 @@
 /** Array methods:
  * 1. filter()
  * 2. find()
+ * 3. findIndex()
  * 3. map()
  * 4. reduce()
  * 5. slice()
  * 6. splice()
+ * 7. concat(): to add new element at the end; does not change the original array;
+ * 8. push(): to add new element at the end; changes the original array;
  */
 const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
 
-/**find method */
+/**find() method */
 // const result = fruits.find((f) => f === "Lemon");
 const findResult = fruits.find((f) => f === "Lemon" || f === "Apple"); // will return the first condition element only
 // we have to put a call back function
@@ -17,14 +20,14 @@ const findResult = fruits.find((f) => f === "Lemon" || f === "Apple"); // will r
 // if condition true then it will return that element
 console.log("find-method: " + findResult);
 
-/**findIndex method */
+/**findIndex() method */
 // const findIndexResult = fruits.findIndex((fruit) => fruit === "Mango");
 const findIndexResult = fruits.findIndex(
   (fruit) => fruit === "Banana" || fruit === "Orange"
 ); // it returns single and first element index for multiple query
 console.log("findIndex method: " + findIndexResult);
 
-/**filter method */
+/**filter() method */
 const filterResult = fruits.filter(
   (fruit) => fruit === "Orange" || fruit === "Apple"
 );
@@ -32,3 +35,31 @@ const filterResult = fruits.filter(
 // it keeps the main array as original
 console.log("filter method: " + filterResult);
 console.log("Main Array after filter method: " + fruits);
+
+/**slice() method */
+// when we want to cut specific indexed element from an array we use slice();
+// it takes to parameter: start and end index number
+// const sliceResult = fruits.slice(starting index number, ending index number)
+const sliceResult = fruits.slice(2, 4);
+console.log("slice method: " + sliceResult);
+// console.log(fruits); //slice keeps original array as original
+
+/**splice() method */
+// it takes minimum 2 parameter: index number(starting point), number of removing
+// items and we can give 3rd parameter to as we need as new item we want to add;
+// most importantly it changes original array.
+// because it changes the original array. it is best practice to make a copy of main array;
+// const spliceResult = fruits.splice(index, noOfRemovingItems, newItem1, newItem2, newItem3);
+// const spliceResult = fruits.splice(1, 3, "newItem1", "newItem2", "newItem3");
+// console.log("splice result: " + spliceResult);
+// console.log(fruits + " after splice");
+
+/**concat() method */
+// const concatResult = fruits.concat("musambi", "grape", "lichi");
+// console.log("concat method: " + concatResult);
+// console.log(fruits);
+
+/**push() method */
+const pushResult = fruits.push("musambi", "grape", "lichi");
+console.log("push method: " + pushResult); // push returns array length after push();
+console.log(fruits); // it changes original array with new element
